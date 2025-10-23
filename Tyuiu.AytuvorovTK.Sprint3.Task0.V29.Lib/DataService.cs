2408a4ebafa76cc367f26250ba019f6330257765
dimka <dimka556.dim4@gmail.com>
2025-10-23 23:23:59 +0500

    Add project files.

---
+++ Tyuiu.AytuvorovTK.Sprint3.Task0.V29.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.AytuvarovTK.Sprint3.Task0.V29.Lib
+{
+    public class DataService : ISprint3Task0V29
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double sumseries = 0;
+            int i;
+            for (i = startValue; i <= stopValue; i++)
+            {
+                double term = (Math.Pow(value, 2 * i) + 1.0 / (i + 1)) * Math.Cos(value);
+                sumseries += term;
+            }
+            return Math.Round(sumseries, 3);
+        }
+    }
+}
---
+++ Tyuiu.AytuvorovTK.Sprint3.Task0.V29/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using Tyuiu.AytuvorovTK.Sprint3.Task0.V29.Lib;
+
+namespace Tyuiu.AytuvorovTK.Sprint3.Task0.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***********************************************************************************************************************************************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                                                                                                                                            *");
+            Console.WriteLine("***********************************************************************************************************************************************************************************************************************************************");
+
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 10;
+
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+
+            Console.WriteLine("*********************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                        *");
+            Console.WriteLine("*********************************************************************************************************************");
+
+            Console.WriteLine("Сумма рада = " + ds.GetSumSeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
