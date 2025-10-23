e382f5f47155323fddf9dc735b6bd22094b9ec27
dimka <dimka556.dim4@gmail.com>
2025-10-23 23:46:14 +0500

    Task1

---
+++ Tyuiu.AytuvorovTK.Sprint3.Task1.V17/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using Tyuiu.AytuvorovTK.Sprint3.Task1.V17.Lib;
+
+namespace Tyuiu.AytuvorovTK.Sprint3.Task1.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***********************************************************************************************************************************************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                                                                                                                                            *");
+            Console.WriteLine("***********************************************************************************************************************************************************************************************************************************************");
+
+            int value = 1;
+            int startValue = 1;
+            int stopValue = 4;
+
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+
+            Console.WriteLine("*********************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                        *");
+            Console.WriteLine("*********************************************************************************************************************");
+
+            Console.WriteLine("Сумма рада = " + ds.GetMultiplySeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
