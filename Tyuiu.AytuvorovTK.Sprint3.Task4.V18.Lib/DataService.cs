5d38a14c4ed0b7c70e73dbce33443a300ff7fcd8
dimka <dimka556.dim4@gmail.com>
2025-10-24 17:56:44 +0500

    Task4

---
+++ Tyuiu.AytuvorovTK.Sprint3.Task4.V18.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.AytuvorovTK.Sprint3.Task4.V18.Lib
+{
+    public class DataService : ISprint3Task4V18
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double res = 1;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    res *= (Math.Sin(x) / Math.Cos(x)) ;
+                }
+            }
+            return Math.Round(res, 3);
+
+        }
+    }
+}
