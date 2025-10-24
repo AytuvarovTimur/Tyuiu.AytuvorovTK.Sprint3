347670eb137c5d31c6ced00cdbda845a96aac19b
dimka <dimka556.dim4@gmail.com>
2025-10-24 18:01:53 +0500

    Ta4

--- Tyuiu.AytuvorovTK.Sprint3.Task4.V18.Lib/DataService.cs
+++ Tyuiu.AytuvorovTK.Sprint3.Task4.V18.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.AytuvorovTK.Sprint3.Task4.V18.Lib
     {
         public double Calculate(int startValue, int stopValue)
         {
-            double res = 1;
+            double res = 0;
             for (int x = startValue; x <= stopValue; x++)
             {
                 if (x == 0)
@@ -14,7 +14,7 @@ namespace Tyuiu.AytuvorovTK.Sprint3.Task4.V18.Lib
                 }
                 else
                 {
-                    res *= (Math.Sin(x) / Math.Cos(x)) ;
+                    res += Math.Sin(x) / Math.Cos(x) ;
                 }
             }
             return Math.Round(res, 3);
